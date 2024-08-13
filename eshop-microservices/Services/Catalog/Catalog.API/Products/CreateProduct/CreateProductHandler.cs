a6ca7c4a091bf2f4b8738e3c4ada1de2facb2fb6
brickwareharry <brickware.harry@gmail.com>
2024-08-14 07:26:18 +1200

    Add create-product end point for catalog service

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    // Here implements Command Query Responsibility Segregation (CQRS) pattern by using MediatR library
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Create Product entity from command object
+            var product = new Product
+            { 
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price            
+            };
+
+            //TODO:
+            // Save to database
+
+            // return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());//Temporarily create NewGuid
+
+        }
+    }
+}
