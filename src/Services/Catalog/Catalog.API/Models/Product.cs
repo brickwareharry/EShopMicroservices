cfa33e47a82e377e04fe9868170b717393800802
brickwareharry <brickware.harry@gmail.com>
2024-09-24 20:28:42 +1200

    Suppress IDE0028 warning from the Category attribute, since the IDE may not recognize this target-typed new() syntax

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -2,11 +2,15 @@
 {
     public class Product
     {// This is in the domain layer
+
         public Guid Id { get; set; }
         public string Name { get; set; } = default!;
+#pragma warning disable IDE0028
         public List<string> Category { get; set; } = new();
+#pragma warning restore IDE0028
         public string Description { get; set; } = default!;
         public string ImageFile { get; set; } = default!;
         public decimal Price { get; set; }
+
     }
 }
