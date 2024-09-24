77be2b269811f387e95eb4275cf598f169e41994
brickwareharry <brickware.harry@gmail.com>
2024-09-24 20:39:16 +1200

    Add get products end point

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    //public record GetProductsRequest();
+    public record GetProductsResponse(IEnumerable<Product> Products);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                //Send functoin of MediatR sends the query to the Handle function of GetProductsQueryHandler
+                var result = await sender.Send(new GetProductsQuery());
+
+                //using Adapt function of Mapster library maps the result of GetProductsResult from Handle function back to GetProductsResponse above
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
+    }
+
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProducts
+{// Here implements Command Query Responsibility Segregation (CQRS) pattern by using MediatR library
+    public record GetProductsQuery() : IQuery<GetProductsResult>;//IQuery inherites from IRequest of MediaR Library
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        // IDocumentSection is from dependency injection container of Marten Library for database operations
+        // ILogger is from Microsoft.Extensions.Logging. Since .NET 6, this logging library is included in WebApplication.
+        // The logger object is injected by dependency injection container automatically so builder.Services.AddLogging() is not needed.
+        : IQueryHandler<GetProductsQuery, GetProductsResult> //ICommandHandler interites from IRequestHandler of MediaR Library
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+        }
+    }
+
+}
